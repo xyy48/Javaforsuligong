e339253cab7d665829a03439e7488532a59e18a4
xyy48 <2538492638@qq.com>
2020-11-26 17:29:24 +0800

    xyy

---
+++ springbootsell/src/main/java/com/neusoft/springbootsell/repository/OrderMasterRepository.java
@@ -0,0 +1,6 @@
+package com.neusoft.springbootsell.repository;
+import com.neusoft.springbootsell.dataobject.OrderMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderMasterRepository extends JpaRepository<OrderMaster, String> {
+}
